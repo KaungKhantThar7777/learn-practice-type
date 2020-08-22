e355be04adcd0ca98cdf149c0591af9e8a5e8bad
kaungkhantthar7777 <kaungkhantthar@ucsm.edu.mm>
2020-08-22 20:53:35 +0630

    Add timer in game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import {
   StyledGame,
   StyledScore,
@@ -8,6 +9,34 @@ import {
 import { Strong } from "../styled/Utils";
 
 const Game = () => {
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+  const history = useHistory();
+  useEffect(() => {
+    const startTime = new Date();
+    const interval = setInterval(() => {
+      updateTime(startTime);
+    }, 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds < 0) {
+      history.push("/gameOver");
+    }
+  });
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+
+    const passedMs = endTime.getTime() - startTime.getTime();
+    setMs(1000 - (passedMs % 1000));
+    console.log(passedMs);
+    const formatSeconds = Math.floor(seconds - passedMs / 1000);
+    setSeconds(formatSeconds);
+    // console.log(formatSeconds);
+  };
   return (
     <StyledGame>
       <StyledScore>
@@ -15,7 +44,10 @@ const Game = () => {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Timer: <Strong>00:00</Strong>
+        Timer:{" "}
+        <Strong>
+          {String(seconds).padStart(2, "0")}:{String(ms).padStart(3, "0")}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
