e91295883d79f9ec4b2a079140b9cd7a9a8bb5be
kaungkhantthar7777 <kaungkhantthar@ucsm.edu.mm>
2020-08-24 13:59:46 +0630

    Add event listener

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { CTA, StyledHome, StyledH1 } from "../styled/Home";
 import { Accent } from "../styled/Utils";
-const Home = () => {
+const Home = ({ history }) => {
+  useEffect(() => {
+    const handleKeyup = (e) => {
+      if (e.key === "s") {
+        history.push("/game");
+      }
+    };
+    document.addEventListener("keyup", handleKeyup);
+    return () => document.removeEventListener("keyup", handleKeyup);
+    //eslint-disable-next-line
+  }, []);
   return (
     <StyledHome>
       <StyledH1>Ready to Type?</StyledH1>
